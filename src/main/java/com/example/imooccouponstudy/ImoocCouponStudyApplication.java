4c461260df1fb64c22789edbf26c1423eaea1914
ARIOS00 <3588315821@qq.com>
2023-01-21 21:37:31 -0500

    test1

---
+++ src/main/java/com/example/imooccouponstudy/ImoocCouponStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.imooccouponstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocCouponStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocCouponStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imooccouponstudy/ImoocCouponStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imooccouponstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocCouponStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
